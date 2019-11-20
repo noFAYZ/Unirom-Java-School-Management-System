85f4f15f70d504a0b857bb1044d0953b300de608
Faizan Asad <57537470+noFAYZ@users.noreply.github.com>
2019-11-20 21:02:31 +0500

    Add files via upload

---
+++ mydatabase.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "users" ("id" INTEGER PRIMARY KEY, "FName" TEXT, "LName" TEXT, "username" VARCHAR, "password" VARCHAR, "type" TEXT, "address" VARCHAR, "dob" DATE, "gender" TEXT);
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '11','Waqas','Ahmed','wahmed','asadasad','ADMIN',NULL,'null',NULL );
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '13','Faizan','Asad','faizanasad','asadasad','STUDENT',NULL,'null',NULL );
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '14','Usman','Khalid','ukhalid','asadasad','ADMIN',NULL,'null',NULL );
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '15','ammad','amir','ammadamir','asadasad','STUDENT',NULL,'null',NULL );
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '16','Kashif','Ali','kashifali','asadasad','FACULTY',NULL,'null',NULL );
+INSERT INTO "users" ( "id","FName","LName","username","password","type","address","dob","gender" ) VALUES ( '17','Usman','Ali','usmanali','asadasad','FACULTY',NULL,'null',NULL );
